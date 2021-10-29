4f8dad8d8bbccb844c01cf68d19110914af804f0
sunmagical@list.ru <sunmagical@list.ru>
2021-10-29 13:40:09 +0500

    123

---
+++ pz_9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace pz_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            x = y = z = 0;
+            int[,] a = new int[8, 6];
+            int[] b = new int[48];
+            Random q = new Random();
+            for (int i = 0; i != 8; i++)
+            {
+                for (int j = 0; j != 6; j++)
+                {
+                    a[i, j] = q.Next(int.MinValue, int.MaxValue);
+                    Console.Write($"{a[i, j]} \t");
+                    if (a[i, j] > 0)
+                    {
+                        b[x] = a[i, j];
+                        x++;
+                    }
+                    else if (a[i, j] < z)
+                    {
+                        z = a[i, j];
+                    }
+                }
+                Console.WriteLine();
+            }
+            while (x != 0)
+            {
+                y += b[x];
+                x--;
+            }
+            Console.WriteLine($"Ответ: {y * z}");
+            Console.ReadLine();
+        }
+    }
+}
