5ae2c2105d32078a0f93743e8a718f14c3385227
sunmagical@list.ru <sunmagical@list.ru>
2021-11-08 18:26:24 +0500

    123

---
+++ pz_11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace pz_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = "This is a test";
+            Char C = 's';
+            string result = S.Replace("" + C, "" + C + C);
+            Console.WriteLine(result);
+
+        }
+    }
+}
