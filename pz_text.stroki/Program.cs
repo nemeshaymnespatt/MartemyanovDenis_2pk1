16d24c36368c3475aa8698a5912b8b6c2a39796f
sunmagical@list.ru <sunmagical@list.ru>
2021-11-11 22:26:25 +0500

    123

---
+++ pz_text.stroki/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace pz_text.stroki
+{
+    internal class Program
+    {
+            static string[] BubbleSort(string[] str)
+            {
+                string temp;
+                for (int i = 0; i < str.Length; i++)
+                {
+                    for (int k = i + 1; k < str.Length; k++)
+                    {
+                        if (str[i].Length > str[k].Length)
+                        {
+                            temp = str[i];
+                            str[i] = str[k];
+                            str[k] = temp;
+                        }
+                    }
+                }
+                return str;
+            }
+
+            static void Main(string[] args)
+            {
+
+                Console.Write("Введите текст: ");
+                string text = Console.ReadLine();
+
+                string[] words = text.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Console.Write($"Количество слов: {words.Length}\n");
+
+                Console.WriteLine(String.Join(" ", BubbleSort(words)));
+            }
+        }
+    }
+
