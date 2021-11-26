a97975b78e15fcc9adfc850efdd43c7dff9711a3
sunmagical@list.ru <sunmagical@list.ru>
2021-11-26 19:05:28 +0500

    pz_17

---
+++ pz_17/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+
+    class Program
+    {
+        static bool Formule(string abc)
+        {
+            int a = 0;
+            int b = 0;
+            for (int i = 0; i < abc.Length; i++)
+            {
+                if (abc[i] == '(')
+                {
+                    a++;
+                }
+                if (abc[i] == ')')
+                {
+                    b++;
+                }
+            }
+            if (a == b)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string abc = "(a^2)-(b^2) = (a-b)(a+b)";
+            Console.WriteLine(abc);
+            Console.WriteLine(Formule(abc));
+        }
+    }
+}
