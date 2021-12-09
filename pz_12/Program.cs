5d21a9aa852f1a36660990287d39e35b5ca5f990
sunmagical@list.ru <sunmagical@list.ru>
2021-12-09 15:38:11 +0500

    da

---
+++ pz_12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Linq;
+
+namespace pz_12
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] str = new string[4];
+            str[0] = "123";
+            str[1] = "456";
+            str[2] = "789";
+            str[3] = "000";
+
+
+            var zxc = str.OrderBy(a => a.Length);
+
+            foreach (string item in zxc)
+                Console.WriteLine(item);
+        }
+    }
+}
---
+++ pz_15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace pz_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\ICESHINE\Desktop";
+            string subpath = @"new";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            dirInfo.CreateSubdirectory(subpath);
+
+
+            var myFile = File.Create(@"C:\Users\ICESHINE\Desktop\new\f2.txt");
+
+            myFile.Close();
+
+            File.AppendAllText(@"C:\Users\ICESHINE\Desktop\new\f2.txt", File.ReadAllText(@"C:\Users\ICESHINE\Desktop\f1.txt"));
+
+        }
+    }
+}
