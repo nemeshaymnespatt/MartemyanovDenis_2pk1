19acf23b7600500662e0a21dd626e6323fa6a3dc
nemeshaymnespatt <91611694+nemeshaymnespatt@users.noreply.github.com>
2022-05-24 19:11:56 +0500

    пз_9 2 семестр
    события

---
+++ пз_9/пз_9/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+delegate void StopCar();
+
+namespace pz_9
+{
+
+    internal class Car
+    {
+        public int skorost;
+
+        public event StopCar MyEvent;
+
+        public void speed()
+        {
+            for (int i = 0; i < 161; i++)
+            {
+                skorost = i;
+                if (skorost == 81)
+                {
+                    if (MyEvent != null)
+                    {
+                        MyEvent();
+                    }
+                }
+                else if (skorost == 121)
+                {
+                    if (MyEvent != null)
+                    {
+                        MyEvent();
+                    }
+                }
+                Console.WriteLine($"{skorost} км/ч");
+
+            }
+
+        }
+
+
+    }
+
+    class Program
+    {
+        static void carname()
+        { Console.WriteLine("Toyota"); }
+
+        public class Patrolman
+        {
+
+            public void Danger()
+            {
+                Console.WriteLine("Снизьте скорость.");
+            }
+            public void OverDanger()
+            {
+                Console.WriteLine("Это было последнее предупреждение.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Car speedometer = new Car();
+            Patrolman signal = new Patrolman();
+            speedometer.MyEvent += signal.Danger;
+            speedometer.speed();
+            speedometer.MyEvent += signal.OverDanger;
+
+        }
+    }
+}
